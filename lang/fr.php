628768f54f93a7f5c62e4d2ada2a821317554752
Hasina77 <Hasina77@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-06 10:25:47 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wordpress-easy-allopass/trunk@448015 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lang/fr.php
@@ -0,0 +1,67 @@
+<?php
+// Backend
+@define("WEA_PLUGIN", "WP Easy Allopass");
+@define("WEA_SAVE", "Enregistrer les modifications");
+@define("WEA_MODIF", "Modification");
+@define("WEA_CANCEL", "Annuler");
+@define("WEA_GOBACK", "Retour au param&eacute;trage");
+@define("WEA_DEL", "Supprimer le produit");
+@define("WEA_DEL2", "Suppression");
+@define("WEA_CHOOSELANG", "Choose your language"); //inverse
+@define("WEA_YOUR_PROD_ID", "Votre id Produit ");
+@define("WEA_ID_AVLBL_ON", "Id Produit disponible sur");
+@define("WEA_AP_ACCOUNT", "votre compte allopass");
+@define("WEA_IDEXAMPLE", "Exemple ");
+@define("WEA_NEED_HELP", "Besoin d'aide ?");
+@define("WEA_NOT_YET_AP", "Pas encore de compte Allopass ?");
+@define("WEA_CLICK_HERE", "Cliquez-ici");
+@define("WEA_DESCR_PROD", "Description ");
+@define("WEA_DESCR_EXPL", "Texte &quot;par d&eacute;faut&quot; qui s'affichera &agrave; gauche du bouton allopass d&eacute;crivant vos contenus prot&eacute;g&eacute;s.");
+@define("WEA_STATS", "Statistiques vente produit");
+@define("WEA_REF", "R&eacute;f ");
+@define("WEA_NUMBER", "N&deg;");
+@define("WEA_AP_CODE", "Code Allopass");
+@define("WEA_DATE", "Date");
+@define("WEA_ID_POST", "Id Article");
+@define("WEA_STATUT", "Statuts (O=Ok)");
+@define("WEA_NOT_YET_SALE", "Pas encore de vente pour ce produit");
+@define("WEA_YOUR_PROD_LIST", "Liste de vos produits");
+@define("WEA_ID", "id");
+@define("WEA_ID_PROD", "Id Produit");
+@define("WEA_DESCRIPTION", "Description");
+@define("WEA_DATE_UPDATE", "Mis-&agrave;-jour");
+@define("WEA_SALE", "Vente");
+@define("WEA_SHORTCODE", "Shortcode");
+@define("WEA_TTL_EDIT", "Modifier");
+@define("WEA_TTL_STAT", "Statistiques");
+@define("WEA_TTL_DEL", "Supprimer");
+@define("WEA_NOT_YET_PROD", "Pas encore de produit");
+@define("WEA_INSTRUCTION", "Shortcode");
+@define("WEA_TODO", "Pour prot&eacute;ger un contenu sur un de vos articles/pages, veuillez y ins&eacute;rer le code suivant :");
+@define("WEA_FREE_CONTENT", "Contenu libre");
+@define("WEA_PAID_CONTENT", "Contenu prot&eacute;g&eacute;");
+@define("WEA_TTL_NOTES", "Remarques :");
+@define("WEA_NOTES_1", "Le param&egrave;tre <strong>id</strong> (<strong>X</strong> = valeur num&eacute;rique) repr&eacute;sente l'id de votre produit. (Voir colonne &quot;Shortcode&quot; du tableau ci-dessus).<br>Ce param&egrave;tre est obligatoire.");
+@define("WEA_NOTES_2", "Vous pouvez ins&eacute;rer <strong>plusieurs shortcode </strong> dans un m&ecirc;me article ou page.");
+@define("WEA_OTHER_NOTES_1", "Si le param&egrave;tre <strong>id</strong> n'est pas d&eacute;fini ou si aucun produit sur le site"); // Between link
+@define("WEA_OTHER_NOTES_2", "ne correspond &agrave; votre <strong>&quot;id Produit&quot;,</strong> Wordpress affichera un message d'erreur..."); // after link
+@define("WEA_SUPPORT_US", "Ce plugin est gratuit. Mais nous retenons juste 10% de commissions pour entretenir le programme et le serveur propulsant le plugin.<br>Mais vous pouvez aussi t&eacute;l&eacute;charger la <a href='http://www.vitar.123.fr/wea-wp-easy-allopass-download/' target='_blank'>Version PRO</a>. (Vous recevez 100% de vos gains et b&eacute;n&eacute;ficiez d'autres avantages et supports)");
+@define("WEA_MSG_ADD_SUCCESS", "Produit ajout&eacute; ! R&eacute;f :");
+@define("WEA_MSG_ADD_ERROR", "Veuillez entrer un id Allopass valide !");
+@define("WEA_MSG_UPD_SUCCESS", "Produit mis &agrave; jour ! R&eacute;f :");
+@define("WEA_MSG_UPD_ERROR", "Veuillez entrer un id Allopass valide !");
+@define("WEA_MSG_DELPROD_1", "Produit R&eacute;f :");
+@define("WEA_MSG_DELPROD_2", "supprim&eacute; !");
+@define("WEA_DEFAULT_DESCR", "Veuillez entrer ici le texte d&eacute;crivant vos contenus payants");
+@define("WEA_BTN_ADDPROD", "Ajouter le produit");
+@define("WEA_TITLE_BOX", "Ajouter");
+@define("WEA_NOTES_BOX_1", "<b>Remarque</b> : Lorsque vous ajoutez un produit sur le site allopass, mettez toujours comme url de retour ");
+@define("WEA_NOTES_BOX_2", "Le plugin redirigera automatiquement votre client vers la page o&ugrave; il visitait avant l'achat.");
+
+// Frontend
+@define("WEA_BG_PAID_CONTENT", "-- D&eacute;but contenu d&eacute;verrouill&eacute; --");
+@define("WEA_END_PAID_CONTENT", "-- Fin contenu d&eacute;verrouill&eacute; --");
+@define("WEA_WRN_ERROR_CODE", "-- Attention ! Erreur de saisie de(s) code(s)... --");
+@define("WEA_WRN_EXPIRE_CODE", "-- Attention ! Code d'acc&egrave;s expir&eacute; ou invalide... --");
+@define("WEA_WRN_END", "-- Fin message --");
+?>
