6596985d1987b83732909b703f0d0e007ca5ad9a
Hasina77 <Hasina77@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-12 05:55:12 +0000

    3.0.2
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-easy-allopass@696404 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/3.0.2/lang/en.php
@@ -0,0 +1,67 @@
+<?php
+// Backend
+@define("WEA_PLUGIN", "WP Easy Allopass");
+@define("WEA_SAVE", "Save Changes");
+@define("WEA_MODIF", "Edit product");
+@define("WEA_CANCEL", "Cancel");
+@define("WEA_GOBACK", "Return to setup");
+@define("WEA_DEL", "Delete product");
+@define("WEA_DEL2", "Delete product");
+@define("WEA_CHOOSELANG", "Choisir votre langue"); //inverse
+@define("WEA_YOUR_PROD_ID", "Your product Id");
+@define("WEA_ID_AVLBL_ON", "Id Product available on");
+@define("WEA_AP_ACCOUNT", "your allopass account");
+@define("WEA_IDEXAMPLE", "Example ");
+@define("WEA_NEED_HELP", "Need Help?");
+@define("WEA_NOT_YET_AP", "No allopass account?");
+@define("WEA_CLICK_HERE", "Click here");
+@define("WEA_DESCR_PROD", "Description");
+@define("WEA_DESCR_EXPL", "Default text that will be displayed at the left of the allopass button describing your protected content.");
+@define("WEA_STATS", "Product Sales Statistics");
+@define("WEA_REF", "Ref ");
+@define("WEA_NUMBER", "N&deg;");
+@define("WEA_AP_CODE", "Allopass code");
+@define("WEA_DATE", "Date");
+@define("WEA_ID_POST", "Post Id");
+@define("WEA_STATUT", "Status (O=Ok)");
+@define("WEA_NOT_YET_SALE", "There are currently no sales for this product");
+@define("WEA_YOUR_PROD_LIST", "Your products list");
+@define("WEA_ID", "id");
+@define("WEA_ID_PROD", "Product Id");
+@define("WEA_DESCRIPTION", "Description");
+@define("WEA_DATE_UPDATE", "Last update");
+@define("WEA_SALE", "Sales");
+@define("WEA_SHORTCODE", "Shortcode");
+@define("WEA_TTL_EDIT", "Edit");
+@define("WEA_TTL_STAT", "Statistics");
+@define("WEA_TTL_DEL", "Delete");
+@define("WEA_NOT_YET_PROD", "You have not yet added product");
+@define("WEA_INSTRUCTION", "Shortcode");
+@define("WEA_TODO", "To protect paid content on one of your posts/pages, insert the following code:");
+@define("WEA_FREE_CONTENT", "Free content");
+@define("WEA_PAID_CONTENT", "Protected content");
+@define("WEA_TTL_NOTES", "Notes:");
+@define("WEA_NOTES_1", "The parameter <strong>Id</strong> (<strong>X</strong> = numeric value) represents the id of your product. (See column &quot;shortcode&quot; in the above table).<br>This parameter is required.");
+@define("WEA_NOTES_2", "You can insert <strong>several shortcodes</strong> in a single post or page.");
+@define("WEA_OTHER_NOTES_1", "If the parameter <strong>id</strong> is not defined or no product at"); // Between link
+@define("WEA_OTHER_NOTES_2", "corresponds to your <strong>&quot;Product Id&quot;,</strong> WordPress will display an error code..."); // after link
+@define("WEA_SUPPORT_US", "This plugin is free for use. But you can also download the <a href='http://www.vitar.123.fr/wea-wp-easy-allopass-download/' target='_blank'>Pro version</a>.<br>(The PRO version allows you to increase your profits and enjoy other benefits and FREE support.)");
+@define("WEA_MSG_ADD_SUCCESS", "Product added! Ref:");
+@define("WEA_MSG_ADD_ERROR", "Please enter a valid Product Id!");
+@define("WEA_MSG_UPD_SUCCESS", "Product Info updated! Ref :");
+@define("WEA_MSG_UPD_ERROR", "Please enter a valid Product Id!");
+@define("WEA_MSG_DELPROD_1", "Product Ref :");
+@define("WEA_MSG_DELPROD_2", "deleted!");
+@define("WEA_DEFAULT_DESCR", "Please enter here the text description of your paid content");
+@define("WEA_BTN_ADDPROD", "Add the product");
+@define("WEA_TITLE_BOX", "Add product");
+@define("WEA_NOTES_BOX_1", "<b>Note</b>: When you add new product on your allopass account, always set as return URL");
+@define("WEA_NOTES_BOX_2", "After purchase, the WEA plugin will automatically redirect the user to the page that he was visiting before.");
+
+// Frontend
+@define("WEA_BG_PAID_CONTENT", "-- Start unlocked content --");
+@define("WEA_END_PAID_CONTENT", "-- End unlocked content --");
+@define("WEA_WRN_ERROR_CODE", "-- Warning! Invalid code entered --");
+@define("WEA_WRN_EXPIRE_CODE", "-- Warning! Access code expired or invalid ... --");
+@define("WEA_WRN_END", "-- End message --");
+?>
